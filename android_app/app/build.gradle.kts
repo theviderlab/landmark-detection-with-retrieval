ffe73b548e06abdd3e6b731e6ac35cc9e6950b5f
ViderLab <github.account@viderlab.com>
2025-06-10 20:01:04 +0200

    app files

---
+++ android_app/app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.arobjectdetector2"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.arobjectdetector2"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    // Core KTX, Lifecycle, Compose
+    implementation(libs.androidxCoreKtx)
+    implementation(libs.androidxLifecycleRuntimeKtx)
+    implementation(libs.androidxActivityCompose)
+    implementation(platform(libs.androidxComposeBom))
+    implementation(libs.androidxUi)
+    implementation(libs.androidxUiGraphics)
+    implementation(libs.androidxUiToolingPreview)
+    implementation(libs.androidxMaterial3)
+
+    // ARCore y AppCompat
+    implementation(libs.googleArcore)
+    implementation(libs.androidxAppcompat)
+    implementation(libs.material)
+
+    implementation(libs.cameraCore)
+    implementation(libs.cameraCamera2)
+    implementation(libs.cameraLifecycle)
+    implementation(libs.cameraView)
+    implementation(libs.cameraExtensions)
+
+    // OpenCV Android
+    implementation(project(":opencv"))
+
+    // Version catalog – SceneView (si lo usas después)
+    implementation(libs.sceneviewAr)
+
+    // Tests
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidxJunit)
+    androidTestImplementation(libs.androidxEspressoCore)
+    androidTestImplementation(platform(libs.androidxComposeBom))
+    androidTestImplementation(libs.androidxUiTestJunit4)
+    debugImplementation(libs.androidxUiTooling)
+    debugImplementation(libs.androidxUiTestManifest)
+}
---
+++ android_app/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")    // <-- JitPack
+    }
+}
+
+rootProject.name = "ARObjectDetector2"
+include(":app", ":opencv")
+project(":opencv").projectDir = file("opencv")
